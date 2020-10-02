624809cfdb99f537ee6cb25bd280b08acc469c92
Peisong227 <72164882+Peisong227@users.noreply.github.com>
2020-10-02 14:22:23 -0600

    Update Shape.java

--- CMPUT301F20T34/app/src/main/java/com/example/cmput301f20t34/Shape.java
+++ CMPUT301F20T34/app/src/main/java/com/example/cmput301f20t34/Shape.java
@@ -3,5 +3,5 @@ package com.example.cmput301f20t34;
 abstract class Shape {
     private int x;
     private int y;
-    private String colour = pink;
+    private String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 }
