4d010fa6d5fcea6cf7365966d6793339d78e1c1c
Paul Saunders <psaunder@ualberta.ca>
2020-10-01 19:04:03 -0600

    Created 'color' attribute = cyan

--- CMPUT301F20T34/app/src/main/java/com/example/cmput301f20t34/Circle.java
+++ CMPUT301F20T34/app/src/main/java/com/example/cmput301f20t34/Circle.java
@@ -2,6 +2,6 @@ package com.example.cmput301f20t34;
 
 public class Circle extends Shape {
     public Circle() {
-        
+
     }
 }
--- CMPUT301F20T34/app/src/main/java/com/example/cmput301f20t34/Shape.java
+++ CMPUT301F20T34/app/src/main/java/com/example/cmput301f20t34/Shape.java
@@ -3,4 +3,5 @@ package com.example.cmput301f20t34;
 abstract class Shape {
     private int x;
     private int y;
+    private String color = "cyan";
 }
